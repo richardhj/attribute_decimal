e7c763cb4dea35f8a346eab27f160b5f0903e6a5
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-01-10 14:12:53 +0100

    Allow overrides of meta information for widget generating.

--- src/system/modules/metamodelsattribute_decimal/MetaModelAttributeDecimal.php
+++ src/system/modules/metamodelsattribute_decimal/MetaModelAttributeDecimal.php
@@ -20,7 +20,7 @@ if (!defined('TL_ROOT'))
 
 /**
  * This is the MetaModelAttribute class for handling text fields.
- * 
+ *
  * @package	   MetaModels
  * @subpackage AttributeDecimal
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
@@ -53,9 +53,9 @@ class MetaModelAttributeDecimal extends MetaModelAttributeSimple
 		));
 	}
 
-	public function getFieldDefinition()
+	public function getFieldDefinition($arrOverrides = array())
 	{
-		$arrFieldDef=parent::getFieldDefinition();
+		$arrFieldDef=parent::getFieldDefinition($arrOverrides);
 		$arrFieldDef['inputType'] = 'text';
 		$arrFieldDef['eval']['rgxp'] = 'digit';
 		return $arrFieldDef;
