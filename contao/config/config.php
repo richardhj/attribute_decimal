59250ab2c3bc2be567b699f104e6e11641512e59
Cliff Parnitzky <github@cliff-parnitzky.de>
2014-12-15 22:19:49 +0100

    Added missing line

--- contao/config/config.php
+++ contao/config/config.php
@@ -6,6 +6,7 @@
  * data in each collection.
  *
  * PHP version 5
+ *
  * @package     MetaModels
  * @subpackage  AttributeDecimal
  * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
