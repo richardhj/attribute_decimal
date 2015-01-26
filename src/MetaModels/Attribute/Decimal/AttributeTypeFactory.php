40816ed716a55623a0159d56ac476112b4bff63d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-01-26 19:16:03 +0100

    Change to attribute factory.

---
+++ src/MetaModels/Attribute/Decimal/AttributeTypeFactory.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ *
+ * @package    MetaModels
+ * @subpackage AttributeDecimal
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace MetaModels\Attribute\Decimal;
+
+use MetaModels\Attribute\AbstractAttributeTypeFactory;
+
+/**
+ * Attribute type factory for decimal attributes.
+ */
+class AttributeTypeFactory extends AbstractAttributeTypeFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->typeName  = 'decimal';
+        $this->typeIcon  = 'system/modules/metamodelsattribute_decimal/html/decimal.png';
+        $this->typeClass = 'MetaModels\Attribute\Decimal\Decimal';
+    }
+}
